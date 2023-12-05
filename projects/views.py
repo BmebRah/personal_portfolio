e3d17c556065b47af7131d13397339a82a4b8479
mebrah <mebrahtubereket@gmail.com>
2023-12-05 18:22:22 +0000

    Adds url patterns for project index and project detail views

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
+from projects.models import Project
 
-def project(request):
-    return render(request, "pages/projects.html", {})
+
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        "projects": projects
+    }
+    return render(request, "projects/project_index.html", context)
+
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        "project": project
+    }
+
+    return render(request, "projects/project_detail.html", context)
