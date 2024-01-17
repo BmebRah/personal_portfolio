58f7974d9e76603ab47fbd093d68f26eb3b4e5ed
Bereket Mebrahtu <142580899+BmebRah@users.noreply.github.com>
2024-01-17 19:00:57 +0000

    Tests plan workflow (#1)
    Adds github actions workflow to automate terraform plan and apply

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "~>5.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "eu-west-2"
+}
+
+resource "aws_ecr_repository" "backend" {
+    name                 = "portfolio-aws-backend"
+    image_tag_mutability = "MUTABLE"
+
+}
