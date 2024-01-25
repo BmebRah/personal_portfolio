c5788ce232c1cade5678d92ec39f1631400c32f9
Bereket <mebrahtubereket@gmail.com>
2024-01-25 20:55:21 +0000

    Updates ecs.tf

--- ecs.tf
+++ ecs.tf
@@ -1,4 +1,4 @@
-# Production cluster 
+# Production cluster
 resource "aws_ecs_cluster" "prod" {
   name = "prod"
 }
@@ -27,7 +27,7 @@ resource "aws_ecs_task_definition" "prod_backend_web" {
 }
 
 resource "aws_ecs_service" "prod_backend_web" {
-  name                               = "pro-backend-web"
+  name                               = "prod-backend-web"
   cluster                            = aws_ecs_cluster.prod.id
   task_definition                    = aws_ecs_task_definition.prod_backend_web.arn
   desired_count                      = 1
@@ -35,7 +35,6 @@ resource "aws_ecs_service" "prod_backend_web" {
   deployment_maximum_percent         = 200
   launch_type                        = "FARGATE"
   scheduling_strategy                = "REPLICA"
-
 
   load_balancer {
     target_group_arn = aws_lb_target_group.prod_backend.arn
@@ -52,26 +51,25 @@ resource "aws_ecs_service" "prod_backend_web" {
 
 # Security Group
 resource "aws_security_group" "prod_ecs_backend" {
-  name   = "prod-ecs-backend"
-  vpc_id = aws_vpc.prod.id
+  name        = "prod-ecs-backend"
+  vpc_id      = aws_vpc.prod.id
 
   ingress {
-    from_port      = 0
-    to_port        = 0
-    protocol       = -1
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
     security_groups = [aws_security_group.prod_lb.id]
   }
 
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = -1
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# Iam roles and policies
-
+# IAM roles and policies
 resource "aws_iam_role" "prod_backend_task" {
   name = "prod-backend-task"
 
@@ -113,18 +111,16 @@ resource "aws_iam_role" "ecs_task_execution" {
 resource "aws_iam_role_policy_attachment" "ecs-task-execution-role-policy-attachment" {
   role       = aws_iam_role.ecs_task_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-
 }
 
 # Cloudwatch Logs
-
 resource "aws_cloudwatch_log_group" "prod_backend" {
-    name     = "prod-backend"
-    retention_in_days = var.ecs_prod_backend_retention_days
-
+  name              = "prod-backend"
+  retention_in_days = var.ecs_prod_backend_retention_days
 }
 
 resource "aws_cloudwatch_log_stream" "prod_backend_web" {
-    name = "pro-backend-web"
-    log_group_name = aws_cloudwatch_log_group.prod_backend.name
+  name           = "prod-backend-web"
+  log_group_name = aws_cloudwatch_log_group.prod_backend.name
 }
+
