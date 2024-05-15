9cf4364e7d12df43e613993a944f4bd835e71cda
bmebrah <mebrahtubereket@gmail.com>
2024-05-15 14:17:22 +0100

    Attaches secutity group to an instance

--- security_group.tf
+++ security_group.tf
@@ -1,23 +1,13 @@
 resource "aws_security_group" "allow_tls" {
-    name        = "awws_tls"
-    description = "Allows TLS inbound traffic and all outbound traffic"
+    name        = "allow_tls"
     vpc_id      = aws_vpc.main.id
 
-    tags = {
-        Name    = "allws_tls"
-    }
 
-    ingress = {
+    ingress {
         from_port = 0
         to_port   = 0
-        protocol  = "-1" 
-        security_groups = ["aws_security_group.allws_tls.id"]
+        protocol  = "tcp" 
+        cidr_blocks = ["0.0.0.0/0"]
     }
-    egress = {
-        from_port = 0
-        to_port   = 0
-        protocol  = "-1"
-        cidr_block = "0.0.0.0/0"
 
-    }
 }
